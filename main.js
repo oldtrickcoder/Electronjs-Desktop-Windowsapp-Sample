1be046088763d9f416f286e1706ead4fc32bd911
oldtrickcoder <ypratama66@gmail.com>
2021-02-12 21:35:51 +0700

    First Commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, ipcMain, Notification } = require("electron");
+const path = require("path");
+
+const loadMainWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width : 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.on("ready", loadMainWindow);
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+      app.quit();
+    }
+  });
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        loadMainWindow();
+    }
+});
+
+ipcMain.handle('show-notification', (event, ...args) => {
+    const notification = {
+        title: 'New Task',
+        body: `Added: ${args[0]}`
+    }
+
+    new Notification(notification).show()
+});
